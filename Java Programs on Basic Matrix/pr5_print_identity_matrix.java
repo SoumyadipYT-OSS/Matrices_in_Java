146eb79a58bd7e0e39bc87636e910d2f2a393229
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-09-03 21:18:49 +0530

    Add files via upload

---
+++ Java Programs on Basic Matrix/pr5_print_identity_matrix.java
@@ -0,0 +1,32 @@
+package Applied_Linear_Algebra;
+
+import java.util.Scanner;
+public class pr5_print_identity_matrix 
+{
+	private static int identityMatrix(int num)
+	{
+		int row=0, column=0;
+		for (row=0; row<num; row++)
+		{
+			for (column=0; column<num; column++)
+			{
+				if (row==column)
+					System.out.print(1+" ");
+				else
+					System.out.print(0+" ");
+			}
+			System.out.println();
+		}
+		return 0;
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input a number to see its identity matrix: ");
+		int num = sc.nextInt();
+		identityMatrix(num);
+		sc.close();
+	}
+}
