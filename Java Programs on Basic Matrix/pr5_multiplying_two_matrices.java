b307bb7c2abd3d6c798bd0eee98c08cab960be09
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-09-03 21:23:53 +0530

    Add files via upload

---
+++ Java Programs on Basic Matrix/pr5_multiplying_two_matrices.java
@@ -0,0 +1,69 @@
+package Applied_Linear_Algebra;
+
+import java.util.Scanner;
+
+public class pr5_multiplying_two_matrices 
+{
+	private static void print2dArray(int[][] matrix)
+	{
+		for (int r=0; r<=matrix.length-1; r++)
+		{
+			for (int c=0; c<=matrix.length-1; c++)
+			{
+				System.out.print(matrix[r][c]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	private static void multiplying_of_matrix(int[][] first, int[][] second, int row, int column)
+	{
+		int r, c;
+		int addition[][] = new int[row][column];
+		for (r=0; r<row; r++)
+		{
+			for (c=0; c<column; c++)
+			{
+				addition[r][c] = first[r][c]*second[r][c];
+			}
+		}
+		print2dArray(addition);
+	}
+	
+	
+	@SuppressWarnings({"resource"})
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter row: ");
+		int row = sc.nextInt();
+		System.out.print("Enter column: ");
+		int column = sc.nextInt();
+		int[][] firstMatrix = new int[row][column];
+		int[][] secondMatrix = new int[row][column];
+		System.out.println("• Enter values of first matrix");
+		for (int r=0; r<row; r++)
+		{
+			for(int c=0; c<column; c++)
+			{
+				System.out.print(String.format("Enter [%d][%d]th element: ", r,c));
+				firstMatrix[r][c] = sc.nextInt();
+			}
+		}
+		System.out.println("• Enter values of second matrix");
+		for (int r=0; r<row; r++)
+		{
+			for(int c=0; c<column; c++)
+			{
+				System.out.print(String.format("Enter [%d][%d]th element: ", r,c));
+				secondMatrix[r][c] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("• The first matrix");
+		print2dArray(firstMatrix);
+		System.out.println("• The second matrix");
+		print2dArray(secondMatrix);
+		System.out.println("• The multiplication of these two matrices →");
+		multiplying_of_matrix(firstMatrix, secondMatrix,row, column);
+	}
+}
